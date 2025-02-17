d52d5e65dd7a64bbf3ce34e338d9983119deed9e
Mark Peters <mapeters101@earthlink.net>
2025-02-17 16:52:48 -0500

    Work on lesson017 from 4:05.  class Sunrise.

--- lesson017.py
+++ lesson017.py
@@ -16,6 +16,23 @@ class Celsius:
         print("Setting temperature")
         self._temperature = value
 
+class Sunrise:
+    sunrise_hour = None
+
+    def __init__(self, day="Sunday"):
+        self.day = day
+
+    @staticmethod
+    def set_today(today): # Can't access class or instance attributes or methods
+        # self.day = today # Will throw an error
+        return today
+
+    @classmethod
+    def set_sunrise_class_method(cls, hour): # Can access class attributes & methods
+        cls.sunrise_hour = hour
+
+    def set_sunrise(self, hour):
+        self.sunrise_hour = hour  # Will create instance attribute but will not change class attribute
 
 
 if __name__ == '__main__':
